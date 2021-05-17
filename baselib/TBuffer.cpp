af0f4237556d9af3d728a135b47f437d2174c908
octerboy <octerboy@gmail.com>
2021-05-17 02:01:16 +0000

    no TBINLINE

--- baselib/TBuffer.cpp
+++ baselib/TBuffer.cpp
@@ -10,11 +10,11 @@
 /**
  Title: 	TBuffer
  Build: created by octerboy, 2005/06/10
- $Id: baselib/TBuffer.cpp 2021-04-27 07:28:18 +0000  octerboy $
+ $Id$
 */
 
-#define TEXTUS_MODTIME  "$Date:   2021-04-27 07:28:18 +0000 $"
-#define TEXTUS_BUILDNO  "$Revision:        5 $"
+#define TEXTUS_MODTIME  "$Date$"
+#define TEXTUS_BUILDNO  "$Revision$"
 #include "hook.h"
 
 #if defined(_WIN32) 
