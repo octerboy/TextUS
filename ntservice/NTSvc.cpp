1d3781a13395f0884d1ef4f7c4b272b5744fa7c1
octerboy <octerboy@gmail.com>
2021-12-08 07:25:44 +0000

    add register code

--- ntservice/NTSvc.cpp
+++ ntservice/NTSvc.cpp
@@ -21,23 +21,32 @@
 #include "Amor.h"
 #include "Notitia.h"
 #include "textus_string.h"
+#include "resource.h"
+#include "casecmp.h"
 #include <time.h>
 #include <psapi.h>
 #include <string.h>
 #include <assert.h>
+#include <windows.h>
+#include <iphlpapi.h>
+#include "md5.h"
+
 #pragma comment(lib,"advapi32.lib")
+#pragma comment(lib, "IPHLPAPI.lib")
+INT_PTR CALLBACK	MyRegProc(HWND, UINT, WPARAM, LPARAM);
+#define AUTH_CODE_ATTR "ntservice_code"
 
 void error_pro(const char* msg)
 {
-    char errstr[1024], dispstr[2048];
-    DWORD dw = GetLastError();
+	char errstr[1024], dispstr[2048];
+	DWORD dw = GetLastError();
 
-    FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, dw,
-        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        errstr, 1024, NULL);
+	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, dw,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		errstr, 1024, NULL);
 
-    wsprintf(dispstr, "%s with error %d: %s", (char*)msg, dw, errstr);
-    MessageBox(NULL, (const char*)dispstr, TEXT("Error"), MB_OK);
+	wsprintf(dispstr, "%s with error %d: %s", (char*)msg, dw, errstr);
+	MessageBox(NULL, (const char*)dispstr, TEXT("Error"), MB_OK);
 }
 
 class NTSvc :public Amor
@@ -88,6 +97,20 @@ public:
 	bool uninstall();
 	bool install(const char *cfg);
 	bool isinstalled();
+	bool ifree;
+	char u_reg_code[32];	//来自xml中的注册码
+	bool get_authed();
+
+	void get_reg_str();
+	bool get_machine_str();
+	bool putRegCode();
+
+	TiXmlElement* reg_root ;	//注册文件
+	const char* cfg_sum_string ;
+	char machin_str[64];
+	char reg_m_str[32];
+	bool has_authed ;
+
 #include "wlog.h"
 };
 #define EVMSG_INSTALLED                  0x00000064L
@@ -106,13 +129,70 @@ static void WINAPI serviceMain(DWORD dwArgc, LPTSTR* lpszArgv);
 static void WINAPI handler(DWORD dwOpcode);
 NTSvc *NTSvc::here_svc = (NTSvc*)0;
 
+INT_PTR CALLBACK MyRegProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HWND he;
+	char msg[1024];
+	UNREFERENCED_PARAMETER(lParam);
+
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		//MessageBox(NULL, " MyRegProc.", TEXT("Error"), MB_OK);
+		he = GetDlgItem(hDlg, IDC_NTSERVICE_MACH_STR);
+		SendMessage(he, WM_SETTEXT, 0, (LPARAM)NTSvc::here_svc->machin_str);
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			he = GetDlgItem(hDlg, IDC_NTSERVICE_REG_STR);
+			SendMessage(he, WM_GETTEXT, sizeof(msg), (LPARAM)msg);
+			if (strcasecmp(msg, NTSvc::here_svc->reg_m_str) == 0)
+			{
+
+				if (NTSvc::here_svc->putRegCode())
+				{
+					NTSvc::here_svc->has_authed = true;
+					MessageBox(NULL, "Register successfully!", TEXT("NT Service"), MB_OK);
+					EndDialog(hDlg, LOWORD(wParam));
+				} else {
+					MessageBox(NULL, "Register failed on writing!", TEXT("NT Service"), MB_OK);
+				}
+			}
+			else {
+				MessageBox(NULL, "Register error!", TEXT("NT Service"), MB_OK);
+			}
+			return (INT_PTR)TRUE;
+			break;
+
+		case IDCANCEL:
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+			break;
+
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+
 void NTSvc::ignite(TiXmlElement *cfg) { 
 	const char *comm_str;
 	char *p;
 	char errmsg[64];
+	const char* tmp_c;
+
+	ifree = false;
+	here_svc = this;
 
 	if ( !cfg )
 		return;
+	if ((comm_str = cfg->Attribute("free")))
+		ifree = true; 
+
 	if ( (comm_str = cfg->Attribute("service_name")) )
 		TEXTUS_STRNCPY( service_name, comm_str, sizeof(service_name)-1);
 
@@ -143,33 +223,56 @@ void NTSvc::ignite(TiXmlElement *cfg) {
 		while ( *p != '\\' && *p != '/'  && p != &appPath[0] ) p--;
 		if (  *p == '\\' || *p == '/'  ) *p = 0;
 	}
-}
+
+	reg_root = 0;	//注册文件
+	cfg_sum_string = 0;
+	has_authed = false;
+	reg_root = cfg->GetDocument()->RootElement();
+	tmp_c = reg_root->Attribute(AUTH_CODE_ATTR);
+	memset(u_reg_code, 0, sizeof(u_reg_code));
+	memset(reg_m_str, 0, sizeof(reg_m_str));
+	if (tmp_c)	//这是放注册码的
+	{
+		size_t tlen;
+		tlen = strlen(tmp_c);
+		if ((tlen - 2) > sizeof(u_reg_code))
+			tlen = sizeof(u_reg_code);
+		TEXTUS_STRCPY(u_reg_code, tmp_c);
+	}
+	cfg_sum_string = reg_root->Attribute("sum");
+}	
 
 bool NTSvc::facio( Amor::Pius *pius)
 {
 	void **ps;
 	BOOL ret;
-    char disp[1024];
+	char disp[1024];
 	SERVICE_TABLE_ENTRY st[] = {
 		{service_name, serviceMain},
 		{NULL, NULL}
 	};
 	assert(pius);
-    switch (pius->ordo)
-    {
-    case Notitia::MAIN_PARA:	/* 在整个系统中, 这应是最后被通知到的。 */
-        WBUG("facio Notitia::MAIN_PARA");
-        ps = (void**)pius->indic;
-        if (parseStandardArgs((*(int*)ps[0]) - argOffset, &((char**)ps[1])[argOffset],disp)) {
-                MessageBox(NULL, (const char*)disp, TEXT("Info"), MB_OK);
-               break;
-        }
+	switch ( pius->ordo )
+	{
+	case Notitia::MAIN_PARA:	/* 在整个系统中, 这应是最后被通知到的。 */
+		WBUG("facio Notitia::MAIN_PARA");
+		ps = (void**)pius->indic;
+		if (parseStandardArgs((*(int*)ps[0]) - argOffset, &((char**)ps[1])[argOffset], disp)) {
+			MessageBox(NULL, (const char*)disp, TEXT("Info"), MB_OK);
+			break;
+		}
 		goto SRV_START;
 
 	case Notitia::WINMAIN_PARA:	/* 在整个系统中, 这应是最后被通知到的。 */
 		WBUG("facio Notitia::WinMain_PARA");
 	SRV_START:
-    		WBUG("Calling StartServiceCtrlDispatcher(service_name(%s), serviceMain(%p))", st[0].lpServiceName, st[0].lpServiceProc);
+		
+		if (!has_authed) {
+			WLOG(ERR, "this software not authroized!");
+			break;
+		}
+
+    	WBUG("Calling StartServiceCtrlDispatcher(service_name(%s), serviceMain(%p))", st[0].lpServiceName, st[0].lpServiceProc);
 		ret = StartServiceCtrlDispatcher(st);
 		if ( !ret ) 
 		{
@@ -189,8 +292,18 @@ bool NTSvc::facio( Amor::Pius *pius)
 			WLOG(ERR, "no sched or tpoll");
 			break;
 		}
+		if (!ifree)
+		{
+			this->get_reg_str();
+			if (strlen(u_reg_code) == 0 || strcasecmp(u_reg_code, reg_m_str) != 0)
+			{
+				has_authed = false;
+			}	else  {
+				has_authed = true;
+				WLOG(INFO, "this software authroized!");
+			}
+		}
 
-		
 	default:
 		return false;
 	}
@@ -206,7 +319,7 @@ Amor* NTSvc::clone()
 NTSvc::NTSvc()
 {
 	memset(service_name, 0, sizeof(service_name));
-    here_svc = this;
+    here_svc = 0;
     m_iMajorVersion = 1;
     m_iMinorVersion = 0;
     m_hEventSource = NULL;
@@ -355,7 +468,7 @@ void NTSvc::my_handle(DWORD dwOpcode)
         OnStop();
         m_bIsRunning = FALSE;
         logEvent(EVENTLOG_INFORMATION_TYPE, EVMSG_STOPPED, service_name, " stopped.");
-	//WLOG(INFO, "service %s stopped", service_name);
+	//WLOG(INFO, "service %s stopped", service_name); control panel will halt
         break;
 
     case SERVICE_CONTROL_PAUSE: // 2
@@ -440,6 +553,7 @@ void NTSvc::OnStop()
 	Amor::Pius tmp_pius;
 	tmp_pius.indic = 0;
 	tmp_pius.ordo = Notitia::CMD_MAIN_EXIT;
+	WLOG(INFO, "service %s is stopping", service_name);
 	sch->sponte(&tmp_pius);
 }
 
@@ -516,32 +630,35 @@ void NTSvc::setStatus(DWORD dwState)
 
 // Returns TRUE if it found an arg it recognised, FALSE if not
 // Note: processing some arguments causes output to stdout to be generated.
-bool NTSvc::parseStandardArgs(int argc, char* argv[], char *disp)
+bool NTSvc::parseStandardArgs(int argc, char* argv[], char*disp)
 {
     // See if we have any command line args we recognise
-
     if (argc <= 1) return FALSE;
 
     if (_stricmp(argv[1], "-v") == 0) {
 
         // Spit out version info
-        TEXTUS_SPRINTF(disp,1024,"%s %s Version %d.%d\n", argv[0],
-               service_name, m_iMajorVersion, m_iMinorVersion);
-        TEXTUS_SPRINTF(disp, 1024, "The service is %s installed\n",
-               isinstalled() ? "currently" : "not");
-        return TRUE; // say we processed the argument
+		TEXTUS_SPRINTF(disp, 1024, "%s %s Version %d.%d\n", argv[0],
+			service_name, m_iMajorVersion, m_iMinorVersion);
+		TEXTUS_SPRINTF(disp, 1024, "The service is %s installed\n",
+			isinstalled() ? "currently" : "not");
+		return TRUE; // say we processed the argument
 
     } else if (_stricmp(argv[1], "-i") == 0) {
 
         // Request to install.
+		if (!get_authed()) {
+			return TRUE;
+		}
+
         if (isinstalled()) {
-            TEXTUS_SPRINTF(disp, 1024, "%s is already installed\n", service_name);
+			TEXTUS_SPRINTF(disp, 1024, "%s is already installed\n", service_name);
         } else {
             // Try and install the copy that's running
             if (install(argv[0])) {
-                TEXTUS_SPRINTF(disp, 1024, "%s installed successfully!\n", service_name);
+				TEXTUS_SPRINTF(disp, 1024, "%s installed successfully!\n", service_name);
             } else {
-                TEXTUS_SPRINTF(disp, 1024, "%s failed to install. Error %d\n", service_name, GetLastError());
+				TEXTUS_SPRINTF(disp, 1024, "%s failed to install. Error %d\n", service_name, GetLastError());
             }
         }
         return TRUE; // say we processed the argument
@@ -550,21 +667,20 @@ bool NTSvc::parseStandardArgs(int argc, char* argv[], char *disp)
 
         // Request to uninstall.
         if (!isinstalled()) {
-            TEXTUS_SPRINTF(disp, 1024, "%s is not installed\n", service_name);
+			TEXTUS_SPRINTF(disp, 1024, "%s is not installed\n", service_name);
         } else {
             // Try and remove the copy that's installed
             if (uninstall()) {
                 // Get the executable file path
                 char filePath[_MAX_PATH];
                 ::GetModuleFileName(NULL, filePath, sizeof(filePath));
-                TEXTUS_SPRINTF(disp, 1024, "%s removed. (You must delete the file (%s) yourself.)\n",
+				TEXTUS_SPRINTF(disp, 1024, "%s removed. (You must delete the file (%s) yourself.)\n",
                        service_name, filePath);
             } else {
-                TEXTUS_SPRINTF(disp, 1024, "Could not remove %s. Error %d\n", service_name, GetLastError());
+				TEXTUS_SPRINTF(disp, 1024, "Could not remove %s. Error %d\n", service_name, GetLastError());
             }
         }
         return TRUE; // say we processed the argument
-    
     }
          
     // Don't recognise the args
@@ -586,9 +702,7 @@ bool NTSvc::isinstalled()
     if (hSCM) {
 
         // Try to open the service
-        SC_HANDLE hService = ::OpenService(hSCM,
-                                           service_name,
-                                           SERVICE_QUERY_CONFIG);
+        SC_HANDLE hService = ::OpenService(hSCM, service_name, SERVICE_QUERY_CONFIG);
         if (hService) {
             bResult = TRUE;
             ::CloseServiceHandle(hService);
@@ -596,9 +710,9 @@ bool NTSvc::isinstalled()
 
         ::CloseServiceHandle(hSCM);
     }
-    else {
-        error_pro("OpenSCManager");
-    }
+	else {
+		error_pro("OpenSCManager");
+	}
     
     return bResult;
 }
@@ -611,8 +725,8 @@ bool NTSvc::install(const char *cfg)
                                      NULL, // ServicesActive database
                                      SC_MANAGER_ALL_ACCESS); // full access
     if (!hSCM) {
-        error_pro("OpenSCManager");  return FALSE;
-    }
+		error_pro("OpenSCManager");  return FALSE;
+	};
 
     // Get the executable file path
     // Create the service
@@ -634,7 +748,7 @@ bool NTSvc::install(const char *cfg)
                                          NULL,
                                          NULL);
     if (!hService) {
-        error_pro("CreateService");  
+		error_pro("CreateService");
         ::CloseServiceHandle(hSCM);
         return FALSE;
     }
@@ -688,10 +802,10 @@ bool NTSvc::uninstall()
     SC_HANDLE hSCM = ::OpenSCManager(NULL, // local machine
                                      NULL, // ServicesActive database
                                      SC_MANAGER_ALL_ACCESS); // full access
-    if (!hSCM) { 
-        error_pro("OpenSCManager");
-        return FALSE;
-    }
+	if (!hSCM) {
+		error_pro("OpenSCManager");
+		return FALSE;
+	}
 
     bool bResult = FALSE;
     SC_HANDLE hService = ::OpenService(hSCM,
@@ -706,14 +820,123 @@ bool NTSvc::uninstall()
         }
         ::CloseServiceHandle(hService);
     }
-    else {
-        error_pro("DeleteService");
-    }
-
+	else {
+		error_pro("DeleteService");
+	}
+    
     ::CloseServiceHandle(hSCM);
     return bResult;
 }
 
 NTSvc::~NTSvc() { } 
-#include "hook.c"
 
+bool NTSvc::get_authed()
+{
+	if (NTSvc::here_svc->ifree) return true;
+	if ( !has_authed)
+	{
+		HMODULE hModule;
+		hModule = GetModuleHandle("ntservice.dll");
+		DialogBox(hModule, MAKEINTRESOURCE(IDD_REG_NTSERVICE), GetForegroundWindow(), MyRegProc);
+	}
+	
+	if (!has_authed) {
+		WLOG(ERR, "this software failed to get authroized!");
+	}
+	else {
+		WLOG(INFO, "this software get authroized!");
+	}
+	return has_authed;
+}
+
+bool NTSvc::get_machine_str()
+{
+	static char m_str[64];
+	char amac[32], md[64];
+	bool ok = false;
+	MD5_CTX Md5Ctx;
+	PIP_ADAPTER_INFO anic;
+
+	ULONG ulOutBufLen = sizeof(IP_ADAPTER_INFO);
+	PIP_ADAPTER_INFO pAdapterInfo = (IP_ADAPTER_INFO*)malloc(sizeof(IP_ADAPTER_INFO));
+	if (pAdapterInfo == NULL)
+		return false;
+	// Make an initial call to GetAdaptersInfo to get the necessary size into the ulOutBufLen variable
+	if (::GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == ERROR_BUFFER_OVERFLOW)
+	{
+		free(pAdapterInfo);
+		pAdapterInfo = (IP_ADAPTER_INFO*)malloc(ulOutBufLen);
+		if (pAdapterInfo == NULL)
+			return false;
+	}
+
+	if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == NO_ERROR)
+	{
+		for (anic = pAdapterInfo; anic != NULL; anic = anic->Next)
+		{
+
+			// for eth
+			if (anic->Type != MIB_IF_TYPE_ETHERNET)
+				continue;
+			// maclen 
+			if (anic->AddressLength != 6)
+				continue;
+
+			TEXTUS_SPRINTF(amac, "%02X-%02X-%02X-%02X-%02X-%02X",	int(anic->Address[0]), 	int(anic->Address[1]),	int(anic->Address[2]), int(anic->Address[3]),
+					int(anic->Address[4]), int(anic->Address[5]));
+			//memcpy(mac_string, amac,strlen(amac));
+			ok = true;
+			break;
+		}
+	} else {
+		::MessageBox(0, "Your computer is not found!", "Error", MB_OK);
+	}
+
+	free(pAdapterInfo);
+	if (!ok) return false;
+#define MD_MACH_MAGIC "JSXDAAAd1234ad"
+
+	//MessageBox(NULL, "get mach 222", TEXT("NT service"), MB_OK);
+	MD5Init(&Md5Ctx);
+	MD5Update(&Md5Ctx, MD_MACH_MAGIC, 14);
+	MD5Update(&Md5Ctx, (char*)& amac[0], (int)strlen(amac));
+	MD5Update(&Md5Ctx, cfg_sum_string, 32);
+	MD5Final((char*)& md[0], &Md5Ctx);
+	byte2hex((unsigned char*)&md[0], 2, (char*)machin_str);	//摘要值, 2字符
+	machin_str[4] = '-';
+	byte2hex((unsigned char*)& md[2], 2, (char*)& machin_str[5]);	//摘要值, 2字符
+	machin_str[9] = '-';
+	memcpy(&machin_str[10], &cfg_sum_string[4], 4);
+	machin_str[15] = 0;
+	//MessageBox(NULL, "get mach 333", TEXT("Unigo "), MB_OK);
+	return true;
+}
+
+void NTSvc::get_reg_str()
+{
+	MD5_CTX Md5Ctx;
+	unsigned char md[16];
+#define MD_MACH_REG_MAGIC "A_ntservice"
+	if ( !get_machine_str() ) return;
+	MD5Init(&Md5Ctx);
+	MD5Update(&Md5Ctx, MD_MACH_REG_MAGIC, 11);
+	MD5Update(&Md5Ctx, machin_str, (int)strlen(machin_str));
+	MD5Update(&Md5Ctx, "\n", 1);
+	MD5Final((char*)& md[0], &Md5Ctx);
+	byte2hex(md, 4, &reg_m_str[0]);
+	reg_m_str[8] = 0;
+}
+
+bool NTSvc::putRegCode()
+{
+	if (!reg_root )
+		return false;
+	reg_root->SetAttribute(AUTH_CODE_ATTR, reg_m_str);
+	if (reg_root->GetDocument()->SaveFile())
+		return true;
+	else {
+		::MessageBox(0, reg_root->GetDocument()->ErrorDesc(), "Error", MB_OK);
+		return false;
+	}
+}
+#include "hook.c"
---
+++ ntservice/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供杂项文件.rc 使用
+//
+#define IDD_REG_NTSERVICE                     101
+#define IDC_STATIC               1003
+#define IDC_NTSERVICE_MACH_STR       1004
+#define IDC_NTSERVICE_REG_STR        1005
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
