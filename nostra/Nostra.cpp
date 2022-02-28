20d0c45dd156b4fe60415c9daff6d2b8b74d2a20
octerboy <octerboy@gmail.com>
2022-02-28 17:24:27 +0000

    circle queue for instances

--- nostra/Nostra.cpp
+++ nostra/Nostra.cpp
@@ -21,6 +21,11 @@
 #include "Aptus.h"
 #include "Animus.h"
 #include "textus_string.h"
+#include "Notitia.h"
+#include <string.h>
+#define NOSTRA_MAX 2048 /* 默认最大子实例数，它意味着同时可维持的最多连接数 */
+#define INSTANCE_ADD 3  /* 每次实例的增加数 */
+
 class Nostra : public Aptus {
 public:
 	void ignite_t (TiXmlElement *wood, TiXmlElement *);
@@ -32,95 +37,155 @@ public:
 	~Nostra();
 
 protected:
-	Nostra ***filius;	/* 堆栈,保存空闲子实例,按顺序排放 */
-	int *cursor;	/* 栈顶,初始为-1 */	
-	int *child_num;	/* 已生子对象数, 初始0 */
-
-	int growth_rate;	/* 增长率 */
-	int maxium;	/* 最大子实例数 */
-	bool radical;
-	bool isPoineer;
-	bool isHunter;
-	Nostra *last_neo;	/* 最近一次新建的对象 */
-	int seed;	/* 系统启动时预创建的对象数, 默认1 */
+	struct DoubleList {
+		struct DoubleList *next;
+		struct DoubleList *prev;
+		Nostra *me;
+		DoubleList () {
+			next = prev = this;
+			me = 0;
+		};
+		DoubleList *remove()
+		{
+			DoubleList *list = next;
 	
+			next = next->next;
+			next->prev = this;
+
+			return ((list != this) ? list : (DoubleList *)0);
+		};
+		void	append(DoubleList *list)
+		{
+			list->next = this;
+			list->prev = prev;
+
+			prev->next = list;
+			prev = list;
+		};
+
+	};
+	struct  G_CFG {
+		struct DoubleList que; 
+		struct DoubleList *we; 
+		unsigned int we_num;
+		struct DoubleList **filius;	/* 堆栈,保存空闲子实例,按顺序排放 */
+		int cursor;	/* 栈顶,初始为-1 */	
+		int child_num;	/* 已生子对象数, 初始0 */
+		int growth_rate;	/* 增长率 */
+		int maxium;	/* 最大子实例数 */
+		unsigned radical:1;
+		unsigned isHunter:1;
+		Nostra *last_neo;	/* 最近一次新建的对象 */
+		int seed;	/* 系统启动时预创建的对象数, 默认1 */
+		Amor *get() {	/* 从filius中取出一个空闲对象 */
+			Nostra *idle;
+			Amor *neo = (Amor*) 0;
+			struct DoubleList *an_node = que.remove(); 
+			if ( an_node ) {
+				idle = (Nostra*) an_node->me;
+				idle->status = BUSY;
+				neo= idle->owner;
+			} else {
+				return 0;
+			}
+
+			cursor++;	/* now may be 0, for -1 initially */
+			filius[cursor] = an_node;	/* save the case */
+			//an_node->me = 0 ;
+			return neo;
+		};
+		void store(Nostra *born) { /* new */
+			we[child_num].me = born; /* child_num is 0 intially */
+			que.append(&we[child_num]);
+			child_num++;
+		};
+		bool put(Nostra *busy) {	/* 回收一个对象 */
+			if ( busy->refs > 0 )
+			{	/* 有其它线程还在运行, 此对象不能进入filius, 但状态改变 */
+				busy->status = RECLAIM;
+				return false;
+			}
+	
+			busy->status = IDLE;
+			if ( cursor == -1 )
+			{
+				return false;
+			}
+
+			/* 进入空闲栈 */
+			filius[cursor]->me = busy;
+			que.append(filius[cursor]);
+			filius[cursor] = 0;
+			cursor--;
+			return true;
+		};
+
+		G_CFG ( TiXmlElement *nos_ele ) {
+			const char* rad, *add_str, *max_str, *comm_str;
+			size_t  i;
+			cursor = -1;
+			child_num = 0;
+
+			radical = false;
+			maxium = NOSTRA_MAX;
+			isHunter = false;
+			seed = 1;
+			growth_rate = INSTANCE_ADD;
+
+			add_str = nos_ele->Attribute("increase");
+			if ( add_str && atoi(add_str) >=0 )
+			{/* 实际设置的最大连接数可能很小, 该置如果实时更新只接受更大的值 */
+				growth_rate = atoi(add_str);
+			} 
+
+			comm_str = nos_ele->Attribute("seed");
+			if ( comm_str && atoi(comm_str) >=1 )
+				seed = atoi(comm_str);
+
+			rad = nos_ele->Attribute("radic");
+			if ( rad && strcmp(rad, "yes") == 0)
+				radical = true;
+
+			max_str = nos_ele->Attribute("maxium");
+			if ( max_str && atoi(max_str) > 1 ) /*实际设置值可能很小,该置如果实时更新只接受更大的值*/
+				maxium = atoi(max_str);
+
+			we = new DoubleList[maxium];
+			filius = new  DoubleList* [maxium];
+			memset(filius, 0, sizeof(DoubleList*));
+			/*
+			for ( i = 0 ; i < maxium; i++ )
+			{
+				filius[i]= &we[i];
+			}
+			*/
+		};
+		~G_CFG() {
+			delete[] *filius;
+			delete[] we;
+		};
+	};
+        struct G_CFG *gCFG;
+        bool has_config;
+
 	enum {IDLE, BUSY, RECLAIM } status;
 	int refs;	/* 被其它线程引用参考数 */
-	inline Amor *get();	/* 从filius中取出一个空闲对象 */
-	inline bool put(Nostra *);	/* 回收一个对象 */
 
 #include "tbug.h"
 };
-#include "Notitia.h"
-#include <string.h>
-#define NOSTRA_MAX 2048 /* 默认最大子实例数，它意味着同时可维持的最多连接数 */
-#define INSTANCE_ADD 3  /* 每次实例的增加数 */
-
 void Nostra::ignite_t (TiXmlElement *cfg, TiXmlElement *nos_ele)
 {
-	const char* rad, *add_str, *max_str, *comm_str;
-	int max_old;
-
 	WBUG("this %p, aptus %p, cfg %p", this, aptus,  cfg);
 
 	if ( !nos_ele) return;
-
-	growth_rate = INSTANCE_ADD;
-	add_str = nos_ele->Attribute("increase");
-	if ( add_str && atoi(add_str) >=0 )
-	{/* 实际设置的最大连接数可能很小, 该置如果实时更新只接受更大的值 */
-		growth_rate = atoi(add_str);
-	} 
-
-	comm_str = nos_ele->Attribute("seed");
-	if ( comm_str && atoi(comm_str) >=1 )
-		seed = atoi(comm_str);
-
-	rad = nos_ele->Attribute("radic");
-	if ( rad && strcmp(rad, "yes") == 0)
-		radical = true;
-
-	max_old = maxium;
-	max_str = nos_ele->Attribute("maxium");
-	if ( max_str && atoi(max_str) > max_old ) /*实际设置值可能很小,该置如果实时更新只接受更大的值*/
-		maxium = atoi(max_str);
-	else
-		maxium = NOSTRA_MAX;
+	if ( !gCFG ) 
+	{
+		gCFG = new struct G_CFG(nos_ele);
+		has_config = true;
+	}
 
 	canAccessed = true;	/* 至此可以认为此应用模块需要Nostra */
 	need_spo = true;
-
-	if ( !filius)
-	{
-		filius = new Nostra**;
-		*filius = (Nostra**) 0;
-	}
-
-	if ( !cursor )
-	{
-		cursor = new int;
-		*cursor = -1;
-	}
-
-	if ( !child_num )
-	{
-		child_num = new int;
-		*child_num = 0;
-	}
-
-	if ( *filius )
-	{	/* 这么做是为了能够支持ignite的再次被调用, 从而实现参数的实时更新并生效 */
-		Nostra** neo=  new Nostra* [maxium];
-		memset(neo, 0, maxium * sizeof(Nostra*) );
-		memcpy(neo, *filius, max_old);
-		delete[] (*filius);
-		*filius = neo;
-	} else
-	{	/* 初次调用 */ 
-		*filius = new Nostra *[maxium];
-		memset(*filius, 0, maxium * sizeof(Nostra*));
-	}
-	isPoineer = true;
 }
 
 bool Nostra::facio( Amor::Pius *pius)
@@ -130,14 +195,15 @@ bool Nostra::facio( Amor::Pius *pius)
 	switch(pius->ordo)
 	{
 	case Notitia::IGNITE_ALL_READY:
-		WBUG("facio IGNITE_ALL_READY %d", seed);
-		if ( radical )
-		for ( int i = 1 ; i < seed && i < maxium ; i++)
+		WBUG("facio IGNITE_ALL_READY %d", gCFG->seed);
+		if ( gCFG->radical )
+		for ( int i = 1 ; i < gCFG->seed && i < gCFG->maxium ; i++)
 		{
 			Animus *ans;
-			isHunter = true;
+			gCFG->isHunter = true;
+			//if ( i ==4 ) {printf("her---\n");int *a= 0 ; *a=0;}
 			ans = (Animus*) aptus->clone();
-			isHunter = false;
+			gCFG->isHunter = false;
 			if ( ans ) 
 				ans->info(ready2);
 		}
@@ -171,14 +237,14 @@ bool Nostra::sponte_n (Amor::Pius *pius, unsigned int from)
 
 		if ( refs == 0 && status == RECLAIM)
 		{	/* 待回收, 已经不再被引用, 所就进入空闲组 */
-			put(this);
+			gCFG->put(this);
 		}
 		break;
 
 	case Notitia::DMD_CLONE_OBJ:
 		WBUG("sponte DMD_CLONE_OBJ" );
-		last_neo = 0;
-		if ( radical ) /* 这里是根,所以这里就进行clone() */
+		gCFG->last_neo = 0;
+		if ( gCFG->radical ) /* 这里是根,所以这里就进行clone() */
 		{
 			Animus *ans;
 			ans = (Animus *)aptus->clone();
@@ -189,8 +255,8 @@ bool Nostra::sponte_n (Amor::Pius *pius, unsigned int from)
 			((Aptus*)aptus)->prius->sponte(pius);
 		}
 
-		if (last_neo ) 
-			pius->indic = last_neo->owner;
+		if (gCFG->last_neo ) 
+			pius->indic = gCFG->last_neo->owner;
 		break;
 
 	case Notitia:: CMD_FREE_IDLE:
@@ -201,7 +267,7 @@ bool Nostra::sponte_n (Amor::Pius *pius, unsigned int from)
 			break;
 		}
 
-		if ( !put(this))
+		if ( !gCFG->put(this))
 			pius->indic = 0;
 		break;
 
@@ -214,26 +280,26 @@ bool Nostra::sponte_n (Amor::Pius *pius, unsigned int from)
 			break;
 		}
 
-		if ( (pius->indic = get()) )
+		if ( (pius->indic = gCFG->get()) )
 		{
 			WBUG("new owner is %p", pius->indic);
 			break;
 		}
 
-		if ( *child_num == maxium   )
+		if ( gCFG->child_num == gCFG->maxium   )
 		{	/* 已达最大数, 不再新生了 */
-			WBUG("to limit %d", maxium);
+			WBUG("to limit %d", gCFG->maxium);
 			pius->indic = 0;
 			break;
 		}
 		
-		isHunter = true;	/* 使用isHunter这一标志, 以使clone()函数区分是来自本类的CMD_ALLOC_IDLE
+		gCFG->isHunter = true;	/* 使用isHunter这一标志, 以使clone()函数区分是来自本类的CMD_ALLOC_IDLE
 					请求, 还是来自左节点的clone()。只有本类的CMD_ALLOC_IDLE所要求的, 其clone
 					产生的对象才进入filius */
-		here_num = maxium - *child_num;
-		if ( here_num > growth_rate ) here_num = growth_rate;
+		here_num = gCFG->maxium - gCFG->child_num;
+		if ( here_num > gCFG->growth_rate ) here_num = gCFG->growth_rate;
 		for ( i =0; i < here_num; i++)
-		if ( radical )
+		if ( gCFG->radical )
 		{	/* 这是根, 所以这里就进行clone(), prius不变 */
 			Animus *ans;
 			ans = (Animus *)aptus->clone();
@@ -245,9 +311,10 @@ bool Nostra::sponte_n (Amor::Pius *pius, unsigned int from)
 		}	
 
 		/* 再找空闲实例 */
-		pius->indic = get();
+		pius->indic = gCFG->get();
+		//{printf("her---\n");int *a= 0 ; *a=0;}
 
-		isHunter = false;	/* 标志复原 */
+		gCFG->isHunter = false;	/* 标志复原 */
 		break;
 	default: /* 继续向左传递 */
 		return false;
@@ -259,74 +326,23 @@ Amor *Nostra::clone()
 {	
 	Nostra *child = 0;
 	child = new Nostra();
-	(*child_num)++;
 
 	Aptus::inherit( (Aptus*) child);
 
-	child->growth_rate = growth_rate;
-	child->maxium = maxium;
-
-	child->filius = filius;
-	child->cursor = cursor;
-	child->child_num = child_num;
-	child->radical = radical;
-
-	if (isHunter)
+	child->gCFG = gCFG;
+	if (gCFG->isHunter)
 	{	/* 这个clone()是由CMD_ALLOC_IDLE引起的, 所以要进入filius */
-		put(child);
+		gCFG->store(child);
 	}
 
-	child->seed = seed; 
-	last_neo = child;
+	gCFG->last_neo = child;
 	return  (Amor*)child;
-}
-
-bool Nostra::put (Nostra *busy)
-{
-	if ( busy->refs > 0 )
-	{	/* 有其它线程还在运行, 此对象不能进入filius, 但状态改变 */
-		busy->status = RECLAIM;
-		return false;
-	}
-	
-	busy->status = IDLE;
-	if ( (*cursor) + 1 == maxium   )
-	{	/* 已达最大数, '还'不进去了 */
-		return false;
-	}
-
-	/* 进入空闲栈 */
-	(*cursor)++;
-	(*filius)[*cursor] = busy;
-	return true;
-}
-
-Amor* Nostra::get ()
-{
-	Amor *neo = (Amor*) 0;
-	
-	if ( *cursor >= 0 ) 
-	{
-		Nostra *idle;
-		idle = (*filius)[*cursor];
-		idle->status = BUSY;
-		neo= idle->owner;
-		(*cursor)--;
-	}
-	return neo;
 }
 
 Nostra::Nostra()
 {
-	filius = (Nostra***)0;
-	cursor = (int*) 0;
-	child_num = 0;
-
-	radical = false;
-	maxium = 0;
-	isPoineer = false;
-	isHunter = false;
-	seed = 1;
+	gCFG= 0 ;
+        has_config = false;
 
 	refs = 0;	/* 并未有其它线程引用 */
 	status = IDLE;	/* 初生, 当然没有被用 */
@@ -334,15 +350,9 @@ Nostra::Nostra()
 
 Nostra::~Nostra()
 {
-	if ( isPoineer)
+	if ( has_config)
 	{
-		if( cursor) delete cursor;
-		if ( filius )
-		{
-			if ( *filius )
-				delete[] *filius;
-			delete filius;
-		}
+		delete gCFG;
 	}
 }
 #define TEXTUS_APTUS_TAG { 'M', 'u', 'l', 't','i',0 }
