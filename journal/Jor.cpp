fcc334e7d3287ab6931d3c9177fdf61347cae2f9
octerboy <octerboy@gmail.com>
2023-03-09 17:19:45 +0800

    to be for multi thread

--- journal/Jor.cpp
+++ journal/Jor.cpp
@@ -10,12 +10,12 @@
 /**
  Title: Aptus extension, generate log for all modules and output to a module
  Build: created by octerboy, 2006/04/01, Wuhan
- $Id: logdata/Jor.cpp 2021-04-13 08:47:35 +0000  octerboy $
+ $Id$
 */
 
-#define SCM_MODULE_ID  "$Id: journal/Jor.cpp 2021-04-13 08:47:35 +0000  octerboy $"
-#define TEXTUS_MODTIME  "$Date:   2021-04-13 08:47:35 +0000 $"
-#define TEXTUS_BUILDNO  "$Revision: 7 $"
+#define SCM_MODULE_ID  "$Id$"
+#define TEXTUS_MODTIME  "$Date$"
+#define TEXTUS_BUILDNO  "$Revision$"
 /* $NoKeywords: $ */
 
 #include "Aptus.h"
@@ -34,6 +34,31 @@
 #define GETPID getpid()
 #endif
 
+#if defined (MULTI_PTHREAD) 
+#if defined (_WIN32)
+#include <synchapi.h>
+	typedef CRITICAL_SECTION Spin_Type;
+	#define Do_Spin_Lock(X) EnterCriticalSection(&X);
+	#define Do_Spin_UnLock(X) LeaveCriticalSection(&X);
+	#define Spin_Init(X) InitializeCriticalSectionAndSpinCount(&X, 1000);
+
+#elif  defined(__APPLE__)  
+#include <os/lock.h>
+	typedef os_unfair_lock Spin_Type;
+	#define Do_Spin_Lock(X) os_unfair_lock_lock(&X);
+	#define Do_Spin_UnLock(X) os_unfair_lock_unlock(&X);
+	#define Spin_Init(X) X=OS_UNFAIR_LOCK_INIT;
+#else	/*  for pthread, BSD, Linux, Solaris etc */
+
+#include <sys/types.h>
+#include <pthread.h>
+	typedef pthread_spinlock_t Spin_Type;
+	#define Do_Spin_Lock(X) pthread_spin_lock(&X);
+	#define Do_Spin_UnLock(X) pthread_spin_unlock(&X);
+	#define Spin_Init(X) pthread_spin_init(&X, PTHREAD_PROCESS_SHARED);
+#endif
+#endif
+
 #define LOG_BUF_SIZE 1280	/* 日志内容, 1280字符一行， 应该不少吧 */
 #define LOG_BUF_TMP_SIZE 1024
 class Jor: public Aptus {
@@ -41,14 +66,15 @@ public:
 	void ignite_t (TiXmlElement *wood, TiXmlElement *);
 	Amor *clone();
 	Aptus *clone_p(Aptus *p);
-
-	bool sponte_n ( Amor::Pius *, unsigned int from);
 	bool sponte ( Amor::Pius *);
 
 	Jor();
 	~Jor();
 	
 protected:
+#if defined (MULTI_PTHREAD) 
+	static Spin_Type time_tick_spin;	
+#endif
 	static bool hasPid;		/* 是否要记进程的pid */
 	static bool detect_modified;		/* 是否防篡改内容 */
 	static char hostname[64];	/* 主机名，在 logdata 这个 Attachment 元素中用属性说明 */
@@ -261,176 +287,9 @@ Aptus *Jor::clone_p(Aptus *animus) {
 	TusLogger *o = reinterpret_cast<TusLogger*>(animus->owner);
 	o->instance_id = instance_last_id++;
 	//o->which_jor = pthread_self(); //should from sched, to do
-	o->which_jor = 1;
+	o->which_jor = 0;
 	printf ("-f:owner %p ---- owner %p\n", owner, animus->owner);
 	return (Aptus*)this->clone();
-}
-
-bool Jor::sponte_n ( Amor::Pius *pius, unsigned int from)
-{	/* 在owner向左发出数据前的处理 */
-#if defined(_WIN32) && (_MSC_VER < 1400 )
-	struct _timeb now;
-#else
-	struct timeb now;
-#endif
-	struct tm *tdatePtr;
-#if defined(_MSC_VER) && (_MSC_VER >= 1400 )
-	struct tm tdate;
-#endif
-
-	char timestr[64];
-	char ftmstr[128];
-	char millstr[16];
-
-	char levelstr[16];
-	LogLevel level;
-	char *log_ptr;
-	size_t log_size;
-
-	char log_str_buf[LOG_BUF_SIZE];	
-	char log_tmp_str_buf[LOG_BUF_TMP_SIZE];	
-	char *log_tmp_ptr = 0;
-	char *sp_format;
-	va_list *h_va;
-	Amor::Pius log_pius;    /* for out log */
-	void *indic_arr[3];
-
-	log_pius.ordo = 0;
-	indic_arr[0] = &aliusID;
-	indic_arr[1] = &log_str_buf[0];
-	indic_arr[2] = 0;
-	log_pius.indic = &indic_arr[0];	
-	switch (pius->ordo)
-	{
-#define LOGWHAT(X) case Notitia::LOG_##X: \
-		case Notitia::LOG_VAR_##X: \
-			TEXTUS_STRCPY(levelstr, #X); \
-			level = X; \
-			log_pius.ordo = Notitia::FAC_LOG_##X; \
-		break;
-		LOGWHAT(EMERG)	LOGWHAT(ALERT)	LOGWHAT(CRIT)
-		LOGWHAT(INFO)	LOGWHAT(DEBUG)	LOGWHAT(NOTICE)
-		LOGWHAT(ERR)	LOGWHAT(WARNING)
-#undef LOGWHAT		
- 	default:
-		return false;
-	}
-	
-	if (!channels[0] ) return true;	/* 没有输出的渠道, 返回 */
-	if ( !levelActive[level] ) /* 此level未设置 */
-		return true;
-	
-#if defined(_WIN32) && (_MSC_VER < 1400 )
-	_ftime(&now);
-#else
-	ftime(&now);
-#endif
-#if defined(_MSC_VER) && (_MSC_VER >= 1400 )
-	tdatePtr = &tdate;
-	localtime_s(tdatePtr, &now.time);
-#else
-	tdatePtr = localtime(&now.time);
-#endif
-
-	if ( lastSec == now.time && lastMilli == now.millitm)
-	{
-		serialNo++;
-	} else
-	{
-		lastSec = now.time;
-		lastMilli = now.millitm;
-		serialNo = 0;
-	}
-
-	if ( serialNo == 0 )
-		TEXTUS_SPRINTF(millstr, ".%03d", now.millitm);
-	else 
-		TEXTUS_SPRINTF(millstr, ".%03d.%d", now.millitm,serialNo);
-
-	TEXTUS_STRCPY(ftmstr,"%y-%m-%d %H:%M:%S");
-	strftime(timestr, 64, ftmstr, tdatePtr);
-	TEXTUS_STRCAT(timestr, millstr);
-
-	if ( detect_modified ) 
-	{
-		log_ptr = &log_str_buf[10];
-		log_size = sizeof(log_str_buf)-1-10;
-	} else {
-		log_ptr = &log_str_buf[0];
-		log_size = sizeof(log_str_buf)-1;
-	}
-
-	switch (pius->ordo)
-	{
-		case Notitia::LOG_VAR_EMERG:
-		case Notitia::LOG_VAR_ALERT:
-		case Notitia::LOG_VAR_CRIT:
-		case Notitia::LOG_VAR_ERR:
-		case Notitia::LOG_VAR_WARNING:
-		case Notitia::LOG_VAR_NOTICE:
-		case Notitia::LOG_VAR_INFO:
-		case Notitia::LOG_VAR_DEBUG:
-			sp_format = 0;
-			h_va = (va_list*)pius->indic;
-			sp_format = va_arg(*h_va, char* );
-			TEXTUS_VSNPRINTF(log_tmp_str_buf, LOG_BUF_TMP_SIZE, sp_format, *h_va); 
-			log_tmp_ptr = &log_tmp_str_buf[0];
-			break;
-		case Notitia::LOG_EMERG:
-		case Notitia::LOG_ALERT:
-		case Notitia::LOG_CRIT:
-		case Notitia::LOG_ERR:
-		case Notitia::LOG_WARNING:
-		case Notitia::LOG_NOTICE:
-		case Notitia::LOG_INFO:
-		case Notitia::LOG_DEBUG:
-			log_tmp_ptr = (char*)pius->indic;
-			break;
-		default:
-			break;
-	}
-
-	if ( aliusID >=0 ) 
-		TEXTUS_SNPRINTF(log_ptr, log_size, "<%d>%s %s%s%s", 
-			level+aliusID*8, timestr, hostname, alius, log_tmp_ptr);
-	 else {
-
-		if ( hasPid )
-			TEXTUS_SNPRINTF(log_ptr, log_size, "(%d)<%s>%s %s(%d, %p, %d)%s%s", aliusID,
-				levelstr, timestr, hostname, instance_id, owner, GETPID, alius, log_tmp_ptr);
-		else 
-			TEXTUS_SNPRINTF(log_ptr, log_size, "(%d)<%s>%s %s(%d,%p)%s%s", aliusID,
-				levelstr, timestr, hostname, instance_id, owner, alius, log_tmp_ptr);
-	}
-
-	if ( detect_modified ) 
-	{	/*生成MD5校验值, 若篡改日志内容, 就可发现 */
-		BTool::MD5_CTX Md5Ctx;
-		unsigned char md[16];
-		char md_sum[16];
-		int i,l;
-		
-		l = (int)strlen(log_ptr);
-
-		BTool::MD5Init (&Md5Ctx);
-		BTool::MD5Update (&Md5Ctx, last_md_sum, 8);
-		BTool::MD5Update (&Md5Ctx, log_ptr, l);
-		BTool::MD5Update (&Md5Ctx, md_magic, md_magic_len);
-		BTool::MD5Final ((char *) &md[0], &Md5Ctx);
-		for (i = 0; i < 4; i++)
-		{
-			TEXTUS_SNPRINTF (&md_sum[i * 2], 3, "%02x", md[i]);
-		}
-		memcpy (last_md_sum, md_sum, 8);
-		log_str_buf[0] = '[';
-		log_str_buf[9] = ']';
-		memcpy(&log_str_buf[1], md_sum, 8);
-	}
-
-	for ( int j = 0 ; channels[j]; j++)
-		channels[j]->dextra(&log_pius, 0); /* 输出日志 */	
-
-	return true;
 }
 
 bool Jor::sponte ( Amor::Pius *pius)
@@ -500,7 +359,10 @@ bool Jor::sponte ( Amor::Pius *pius)
 #else
 	tdatePtr = localtime(&now.time);
 #endif
-
+//lastSec is critical
+#if defined (MULTI_PTHREAD) 
+	Do_Spin_Lock(time_tick_spin)
+#endif
 	if ( lastSec == now.time && lastMilli == now.millitm)
 	{
 		serialNo++;
@@ -515,7 +377,10 @@ bool Jor::sponte ( Amor::Pius *pius)
 		TEXTUS_SPRINTF(millstr, ".%03d", now.millitm);
 	else 
 		TEXTUS_SPRINTF(millstr, ".%03d.%d", now.millitm,serialNo);
-
+//here end
+#if defined (MULTI_PTHREAD) 
+	Do_Spin_UnLock(time_tick_spin)
+#endif
 	TEXTUS_STRCPY(ftmstr,"%y-%m-%d %H:%M:%S");
 	strftime(timestr, 64, ftmstr, tdatePtr);
 	TEXTUS_STRCAT(timestr, millstr);
